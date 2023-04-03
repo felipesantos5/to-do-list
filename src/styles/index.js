da1b6e74b972e65c82de8673f91697859b32faca
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-04-03 11:46:19 -0300

    style: add styled components

---
+++ src/styles/index.js
@@ -0,0 +1,123 @@
+import styled from "styled-components";
+
+export const Paper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 500px;
+  min-height: 700px;
+  padding: 22px;
+  margin: auto;
+  margin-botton: 20px;
+
+  background: #ffffff;
+  mix-blend-mode: normal;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+`;
+
+export const Title = styled.h1`
+  font-size: 46px;
+  margin-bottom: 22px;
+  font-weight: 800;
+  font-family: "Roboto", sans-serif;
+`;
+
+export const Form = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 26px;
+
+  input {
+    width: 224px;
+    height: 40px;
+
+    text-align: center;
+
+    padding: 8px 24px;
+
+    background: #ffffff;
+    mix-blend-mode: normal;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border: none;
+    border-radius: 10px;
+  }
+
+  .button-submit {
+    height: 60px;
+    width: 124px;
+
+    background: #7066e0;
+    mix-blend-mode: normal;
+    border: none;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+
+    cursor: pointer;
+
+    &:focus {
+      opacity: 0.8;
+    }
+    &:active {
+      opacity: 0.6;
+    }
+  }
+`;
+
+export const Card = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+
+  color: white;
+  margin-bottom: 16px;
+
+  background: ${(props) => (props.checked ? "#7066e0" : "#F5F5F5")};
+  mix-blend-mode: normal;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
+  border-radius: 10px;
+
+  padding: 22px;
+
+  p {
+    max-width: 320px;
+    word-wrap: break-word;
+    color: ${(props) => (props.checked ? "#FFFFFF" : "#111")};
+    text-decoration: ${(props) => (props.checked ? "line-through" : "none")};
+  }
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  gap: 16px;
+  margin-left: 18px;
+
+  .button-icon {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+
+    width: 36px;
+    height: 36px;
+  }
+
+  i {
+    font-size: 36px;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+`;
