752707ce3e06a87417d086325265f32cc5466a03
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-04-04 16:45:32 -0300

    style: changing the wrapper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Wrapper } from "./styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Wrapper>
+      <App />
+    </Wrapper>
   </React.StrictMode>
 );
